5595dc692f3cdd4dfccecb9e847ed512dc609475
Bastian Pezo <bastian.pezo@haulmerinc.onmicrosoft.com>
2024-04-16 23:14:41 -0400

    Add env file and modified supabase client

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,7 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://sxgobywsaifnczjstxqe.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4Z29ieXdzYWlmbmN6anN0eHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2MDYyNTgsImV4cCI6MjAyODE4MjI1OH0.tWaJUSD7YVcLkWnk5Id1ck3rbBgPy4ofaKB41hfIvuM';
+// Usa variables de entorno para configurar Supabase
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = { supabase };
