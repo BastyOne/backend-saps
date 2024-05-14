06bd74fc86d3e10532106486a1558c8335491ba7
Bastian Pezo <bastian.pezo@haulmerinc.onmicrosoft.com>
2024-05-14 14:13:11 -0400

    change localhost for develop

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use('/api/reunion', reunionRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://192.168.100.81:${PORT}`);
 });
