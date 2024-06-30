1df3545ecc2a404b4cad8773dd307e68fcb683ff
Bastian Pezo <bastian.pezo@haulmerinc.onmicrosoft.com>
2024-06-30 16:34:23 -0400

    adding settings for personal/alumno and anon post on forum

--- routes/alumnosRoutes.js
+++ routes/alumnosRoutes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { addAlumno, getAllAlumnos, getAlumnoById } from '../controllers/alumnosController.js';
+import { addAlumno, getAllAlumnos, getAlumnoById, updateAlumno } from '../controllers/alumnosController.js';
+import upload from '../middleware/uploadMiddleware.js';
 import authenticateToken from '../middleware/authMiddleware.js';
+
 const router = Router();
 
-router.post('/add', authenticateToken, addAlumno);
+router.post('/add', authenticateToken, upload.single('archivo'), addAlumno);
 router.get('/', authenticateToken, getAllAlumnos);
 router.get('/:alumnoId', authenticateToken, getAlumnoById);
+router.put('/:alumnoId', authenticateToken, updateAlumno);
 
 
 export default router;
